d228a6fea330fe270f22ba9155a115d8d7d26b66
Rümeysa Tokur <rtokur11@gmail.com>
2025-02-28 22:44:38 +0300

    Initial Commit

---
+++ NoteApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NoteApp
+//
+//  Created by Rumeysa Tokur on 28.02.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
